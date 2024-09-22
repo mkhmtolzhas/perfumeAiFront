45ac8ed72d870ef3d6789da525e39de0c83a2422
Olzhas Makhmet <mkhmtcore@gmail.com>
2024-09-23 02:09:41 +0500

    designed well

--- src/components/component/AiPromptMobile.tsx
+++ src/components/component/AiPromptMobile.tsx
@@ -44,29 +44,37 @@ const AiPromptMobile = () => {
     
     return (
         <div className='w-full px-[6%] py-6'>
-            <div className="container flex justify-center items-center">
-                <form action="" className='flex justify-center items-center gap-2' onSubmit={sendPrompt}>
-                    <input 
-                        type="text" 
-                        value={prompt} 
-                        onChange={(e) => setPrompt(e.target.value)} 
-                        placeholder='Опишите что вы хотите...' 
-                        className='border-[2px] border-black flex h-10 w-full rounded-md border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50' 
-                    />
-                    <button className='h-10 px-3 bg-[#e2ff42] rounded-md flex justify-center items-center text-black text-xs' type='submit'>
-                        <Sparkles className="h-4 w-4" />
-                    </button>
-                </form>
-            </div>
-            <div className="grid grid-cols-2 gap-3 justify-center px-[6%] py-6 border-[2px] rounded-md mt-5 border-[#e2ff42]" >
-                {suggestions.map((suggestion, index) => (
-                    <button
-                    className='px-2 py-2 text-sm bg-[#e2ff42] text-black rounded-md'
-                    key={index}
-                    onClick={() => handleSuggestionClick(suggestion)}
-                    >{suggestion}
-                    </button>
-                ))}
+            <div className="container px-[6%] py-6 border-[2px] border-black rounded-md grid grid-cols-1 gap-4">
+                <div className="flex justify-center items-center text-center">
+                    <h1 className='text-md w-[70%] font-bold'>Наш ИИ подберет вам идельный парфюм</h1>
+                </div>
+                <div className="flex justify-center items-center">
+                    <form action="" className='flex justify-center items-center gap-2' onSubmit={sendPrompt}>
+                        <input 
+                            type="text" 
+                            value={prompt} 
+                            onChange={(e) => setPrompt(e.target.value)} 
+                            placeholder='Опишите что вы хотите...' 
+                            className='border-[2px] border-black flex h-10 w-full rounded-md border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50' 
+                        />
+                        <button className='h-10 px-3 bg-[#e2ff42] rounded-md flex justify-center items-center text-black text-xs' type='submit'>
+                            <Sparkles className="h-4 w-4" />
+                        </button>
+                    </form>
+                </div>
+                <div className="flex justify-center items-center text-center">
+                    <h1 className='text-md text-center font-bold'>Готовые запросы:</h1>
+                </div>
+                <div className="grid grid-cols-2 gap-3 justify-center px-[9%] rounded-md" >
+                    {suggestions.map((suggestion, index) => (
+                        <button
+                        className='px-2 py-2 text-sm bg-[#e2ff42] text-black rounded-md'
+                        key={index}
+                        onClick={() => handleSuggestionClick(suggestion)}
+                        >{suggestion}
+                        </button>
+                    ))}
+                </div>
             </div>
         </div>
     );
